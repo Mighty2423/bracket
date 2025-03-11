f43cdcfcb80c99c3516c53c5003a11373a9e6cbf
Alton <152930905+Mighty2423@users.noreply.github.com>
2025-03-11 17:04:31 -0600

    Update db_connect.php

--- backend/db_connect.php
+++ backend/db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = getenv("DATABASE_HOST") ?: "db"; // Default to 'db' for Docker networking
-$user = getenv("DATABASE_USER") ?: "root";
-$password = getenv("DATABASE_PASSWORD") ?: "";
-$dbname = getenv("DATABASE_NAME") ?: "VolleyballTournament";
+$host = "bracket.xxxxxxx.us-east-1.rds.amazonaws.com"; // Use your RDS endpoint
+$username = "admin";
+$password = "password";
+$dbname = "bracket";
 $port = getenv("DATABASE_PORT") ?: "3306"; // Ensure port is set
 
 $conn = new mysqli($host, $user, $password, $dbname, $port);
